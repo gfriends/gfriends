67087013f66108f57b873934f2986083e1167a88
Zealer Chen <xinxin8816@gmail.com>
2020-09-23 17:42:01 +0800

    Update Gfriends Inputer.py

--- Gfriends Inputer.py
+++ Gfriends Inputer.py
@@ -244,17 +244,21 @@ def check_update():
 		response.encoding = 'utf-8'
 		if response.status_code != 200:
 			print('× 检查更新失败！返回了一个错误： {}\n'.format(response.status_code))
+			rewriteable_word('按任意键跳过...')
+			os.system('pause>nul')	
 		if version.replace('v','') < loads(response.text)[0]['tag_name'].replace('v',''):
 			print(loads(response.text)[0]['tag_name']+' 新版本发布啦！\n')
 			print(re.search('What\'s New?.*?(?=\r\n<details>)',loads(response.text)[0]['body'],flags=re.S).group(0).replace('*',''))
-			print('请通过如下链接更新：\nhttps://github.com/xinxin8816/gfriends/releases\n')
+			print('请通过如下链接更新：\nhttps://github.com/xinxin8816/gfriends/releases\n')		
+			rewriteable_word('按任意键跳过更新...')
+			os.system('pause>nul')
+			print('即将跳过更新。不推荐跳过更新，如遇问题请及时更新。')
+			time.sleep(5)
 	except:
 		if debug: print(format_exc())
 		print('× 检查更新失败！\n')
-	rewriteable_word('按任意键跳过更新...')
-	os.system('pause>nul')
-	print('即将跳过更新。不推荐跳过更新，如遇问题请及时更新。')
-	time.sleep(5)
+		rewriteable_word('按任意键跳过...')
+		os.system('pause>nul')
 	os.system('cls')
 
 os.system('title Gfriends Inputer '+version)
